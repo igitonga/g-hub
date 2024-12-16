d06c9c0a11489ed518e99b0204230ae5b43a6dc0
igitonga <iangitonga47@gmail.com>
2024-12-16 17:33:48 +0300

    Init commit

---
+++ src/lib/queries.ts
@@ -0,0 +1,35 @@
+import { gql } from "@apollo/client";
+
+export const LIST_GAMES = gql`
+  query GetGames {
+    games {
+      id,
+      title,
+      description,
+      imageUrl,
+      reviews {
+        rating
+      }
+    }
+  }
+`;
+
+export const ADD_GAME = gql`
+    mutation AddGame($game: AddGameInput!) {
+        addGame(game: $game) {
+            title,
+            platform
+        }
+    }
+`;
+
+export const GET_GAME = gql`
+  query GetGame($gameId: ID!) {
+    game(id: $gameId) {
+      title,
+      platform,
+      description,
+      imageUrl
+    }
+  }
+`
